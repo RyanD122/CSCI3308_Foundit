20601ccb63bbc4e0c36c32529cfce57b851fe193
LogicianJones <derLogician@gmail.com>
2017-04-10 18:34:03 -0600

    fixed a bug

--- foundit/foundit.py
+++ foundit/foundit.py
@@ -67,10 +67,10 @@ def search(subreddit="all", postLimit=0, topComLimit=0, topReplyLimit=0, topWord
       try:
 
         #adjust top comments
-        print("Before: " + topCom)
+        print("Before: " + str(topCom))
         score = comment.score
         topCom = adjust(topCom, topComLimit, 1, (comment, score, submission))
-        print("After: " + topCom)
+        print("After: " + str(topCom))
         
         #adjust top replies
         parent = comment.parent()
