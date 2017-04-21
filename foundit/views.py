5035a21cfa90cf27a55efc51d0a57ddd707bcafb
LogicianJones <derLogician@gmail.com>
2017-04-21 12:16:50 -0600

    changed format for compling graphs

--- foundit/views.py
+++ foundit/views.py
@@ -11,30 +11,6 @@ from . import graph
 def index(request):
   return render(request, 'foundit/index.html')
 
-#def results(request, subreddit):
-
-#def index(request):
- #   latest_question_list = Question.objects.order_by('-pub_date')[:5]
-  #  template = loader.get_template('polls/index.html')
-   # context = {
-    #    'latest_question_list': latest_question_list,
-    #}
-    #return HttpResponse(template.render(context, request))
-#or better return render(request, 'polls/index.html', context)
-
-#try:
-# question = Question.objects.get(pk=question_id)
-#except Question.DoesNotExist:
-# raise Http404("Question does not exist")
-#return render(request, 'polls'detail.html', {'question':question})
-
-#shortcut get_object_or_404 exists
-
-###############
-
-#  response = "You're looking at the results of subreddit %s."
-#  return HttpResponse(response % subreddit)
-
 def results(request):
   subreddit = request.GET['subreddit']
   postLimit = request.GET['postLimit']
@@ -56,17 +32,12 @@ def results(request):
   oldestPostList = allList[6]
   activePostList = allList[7]
   
-
-  topWordCounts = []
-  topWords = []
-  for key, value in topWordList:
-    topWordCounts.append(value)
-    topWords.append(key)
-
-  dataSet = (topWordCounts, "Top Words", "Occurances", topWords)
-
-  graphString = graph.renderGraph(dataSet)
+  #compile graph for topWords
+  topWordCounts = [x[0] for x in topWordList]
+  topWords = [x[1] for x in topWordList]
+  topWordsData = (topWordCounts, "Top Words", "Occurances", topWords)
+  topWordsGraph = graph.renderGraph(dataSet)
 
   t = loader.get_template('foundit/results.html')
-  c = Context({ 'subreddit': subreddit, 'topComList' : topComList, 'topWordList' : topWordList, 'Graph' : graphString })
+  c = Context({ 'subreddit': subreddit, 'topComList' : topComList, 'topWordsGraph' : topWordsGraph, })
   return HttpResponse(t.render(c))
