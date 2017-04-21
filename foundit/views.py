c6725d12b8c6a75b5d9e63568bcf2bc13cdf8afb
Ryan <derLogician@gmail.com>
2017-04-21 13:45:46 -0600

    trying to fix bug

--- foundit/views.py
+++ foundit/views.py
@@ -40,7 +40,8 @@ def results(request):
 
   #compile graph for topUsers
   topUserCounts = [x[1] for x in topUserList]
-  topUsers = [x[0] for x in topUserList]
+  topUsers = [x[0].name for x in topUserList]
+aasdfsdfads
   topUsersData = (topUserCounts, "Top Users", "Activity", topUsers)
   topUsersGraph = graph.renderGraph(topUsersData)
 
