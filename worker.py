d8c27ff26f4e3b19616b4694349f7e8a817c0bcf
Ryan <derLogician@gmail.com>
2017-04-25 22:06:47 -0600

    more debug stuff, worker doesn't see be workering remotely

--- worker.py
+++ worker.py
@@ -11,5 +11,6 @@ conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
   with Connection(conn):
+    print("hello from worker!")
     worker = Worker(map(Queue, listen))
     worker.work()
