8ef88889617e60ed9ae4a7c8b9e38fe10be3c2f9
Luke Nguyen <lung9198@colorado.edu>
2017-05-03 06:40:07 -0600

    changes to queue....syntax

--- foundit/foundit.py
+++ foundit/foundit.py
@@ -124,7 +124,7 @@ def search(subreddit, postLimit, topComLimit, topReplyLimit, topWordLimit, topUs
 	for submission in reddit.subreddit(subreddit).hot(limit=endpos):
 		if(index>=startpos):
 			pcounter=(float(index-startpos)/float(endpos-startpos))*100
-			print("Searching post: " + str(index)+" - "+"WORKER: "+str(qindex)" - "+str(pcounter)+"%")
+			print("Searching post: " + str(index)+" - "+"WORKER: "+str(qindex)" - "+str(pcounter)+" % ")
 			#add nouns to dictionary
 			tokens = nltk.word_tokenize(submission.title)
 			tagged = nltk.pos_tag(tokens)
